beab8ab7ec542a4c0172f4f1c373dadd29c6d9dc
Amandine Robert <amandine.rob.26@gmail.com>
2019-02-08 09:05:34 +0100

    Tentative pour resoudre un conflit

--- src/nf/Main.java
+++ src/nf/Main.java
@@ -1,5 +1,4 @@
 package nf;
-import java.sql.*;
 
 public class Main {
 
@@ -7,48 +6,5 @@ public class Main {
         System.out.println("Hello World!");
         //test amandine
         System.out.println("jecris du test !");
-
-    }
-
-    public static Connection connectionDB(){
-        Connection connection =null;
-
-        try{
-            connection=ConnectionDB.getConnection();
-            if(connection!= null){
-                System.out.println("Connection established");
-            }
-
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-        finally {
-            if(connection!= null){
-                try {
-                    connection.close();
-                }catch(SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-
-    public static class ConnectionDB{
-
-        public Connection getConnection(){
-            Connection connection =null;
-
-            try{
-                Class.forName("ProjetSIR/lib/mysql-connector-java-8.0.14.jar");
-                System.out.println("Driver oki");
-                connection.DriverManager.getConnection("localhostDB","userName","password");
-            }
-            catch (Exception e){
-                e.printStackTrace();
-            }
-            return connection;
-        }
     }
 }
