431984480a0f55fcf821e1fda4443efd5b0cbdf9
itsRupy <rupertandrewsc@gmail.com>
2019-03-20 00:44:18 +0100

    hbj

--- src/nf/Connexion.java
+++ src/nf/Connexion.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class Connexion {
 
     private Connection con;
-    private static String url = "jdbc:mysql://localhost:3306/sir";
+    private static String url = "jdbc:mysql://db4free.net:3306/itsrupy";
     private static String driver = "com.mysql.cj.jdbc.Driver";
     private String argument = "?serverTimezone=UTC";
 
